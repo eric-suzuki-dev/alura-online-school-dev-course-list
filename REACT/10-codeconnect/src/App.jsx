3c4095e700aebe306a767f116f60c9e25fa61cd3
Eric Yuji Suzuki <142278417+eric-suzuki-dev@users.noreply.github.com>
2024-06-06 01:23:51 -0300

    codeconnect
    React: configurando e estruturando projetos com Vite.

---
+++ REACT/10-codeconnect/src/App.jsx
@@ -0,0 +1,49 @@
+import './App.css';
+import BarraDePesquisa from './components/BarraDePesquisa';
+import Sidebar from './components/Sidebar';
+import Filtro from './components/Filtro';
+import Ordenacao from './components/Ordenacao';
+import Card from './components/Card';
+import { useEffect, useState } from 'react';
+
+function App() {
+	const [dados, setDados] = useState([]);
+	useEffect(() => {
+		fetch(
+			'https://my-json-server.typicode.com/MonicaHillman/codeconnect-api/publicacoes'
+		)
+			.then((resposta) => resposta.json())
+			.then((dados) => setDados(dados));
+	}, []);
+
+	return (
+		<div className='container'>
+			<Sidebar />
+			<div>
+				<BarraDePesquisa />
+				<Filtro />
+				<Ordenacao />
+				<ul className='lista-cards'>
+					{dados
+						? dados.map((item, index) => (
+								<li key={index}>
+									<Card
+										id={item.id}
+										imagemUrl={item.imagem_capa}
+										titulo={item.titulo}
+										resumo={item.resumo}
+										linhasDeCodigo={item.linhas_de_codigo}
+										compartilhamentos={item.compartilhamentos}
+										comentarios={item.comentarios}
+										usuario={item.usuario}
+									/>
+								</li>
+						  ))
+						: null}
+				</ul>
+			</div>
+		</div>
+	);
+}
+
+export default App;
