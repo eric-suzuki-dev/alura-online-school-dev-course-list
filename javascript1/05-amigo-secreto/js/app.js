13d2f4d1f973bb9600be76b987e4f58d7f7b30ef
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-07-10 22:24:33 -0300

    update

---
+++ javascript1/05-amigo-secreto/js/app.js
@@ -0,0 +1,65 @@
+let amigos = [];
+
+function adicionar() {
+    let amigo = document.getElementById('nome-amigo');
+
+    if (amigo.value == "") {
+        alert('Informe o nome de um amigo!');
+        return;
+    }
+
+    if (amigos.includes(amigo.value)) {
+        alert('Nome ja adicionado!');
+        return;
+    }
+
+    let lista = document.getElementById('lista-amigos');
+
+    // Adicionar o nome no Array
+    amigos.push(amigo.value);
+
+    if (lista.textContent == '') {
+        lista.textContent = amigo.value;
+    } else {
+        lista.textContent = lista.textContent + ', ' + amigo.value;
+    }
+
+    amigo.value = '';
+}
+// Função de sorteio
+function sortear() {
+    if (amigos.length < 4) {
+        alert('Adicione pelo menos 4 amigos');
+        return;
+    }
+
+    embaralha(amigos);
+    let sorteio = document.getElementById('lista-sorteio');
+
+    for (let i = 0; i < amigos.length; i++) {
+
+        if (i == amigos.length - 1) {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[0] + '<br>';
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[i + 1] + '<br>';
+        }
+    }
+}
+
+// Função de embaralhar array 
+function embaralha(lista) {
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        [lista[indice - 1], lista[indiceAleatorio]] =
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+// Função reiniciar
+function reiniciar() {
+    amigos = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
+}
