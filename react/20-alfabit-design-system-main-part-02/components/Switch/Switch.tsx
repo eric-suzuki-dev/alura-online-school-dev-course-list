0276f9720a03b5b94af13c8adcd45c80583fae1d
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-07-19 23:52:49 -0300

    Module 04.lesson 02

--- react/20-alfabit-design-system-main-part-02/components/Switch/Switch.tsx
+++ react/20-alfabit-design-system-main-part-02/components/Switch/Switch.tsx
@@ -1,22 +1,48 @@
 import {
-    Switch as HeadlessSwitch,
-    type SwitchProps as HeadlessSwitchProps,
-  } from "@headlessui/react";
-  
-  export type SwitchProps = {
-    defaultEnable?: boolean;
-    variant?: "common" | "contract";
-    disabled?: boolean;
-    onChange?: (enabled: boolean) => void;
+  Switch as HeadlessSwitch,
+  type SwitchProps as HeadlessSwitchProps,
+} from "@headlessui/react";
+import { useState } from "react";
+import { CheckIcon, XMarkIcon } from "@heroicons/react/24/solid";
+
+export type SwitchProps = {
+  defaultEnable?: boolean;
+  variant?: "common" | "contract";
+  disabled?: boolean;
+  onChange?: (enabled: boolean) => void;
+};
+
+const Switch = ({
+  defaultEnable: enabledByDefault,
+  variant = "common",
+  disabled,
+  onChange,
+  ...rest
+}: SwitchProps) => {
+  const [enabled, setEnabled] = useState(enabledByDefault);
+
+  const toggle = () => {
+    const newState = !enabled;
+    setEnabled(newState);
+    onChange?.(newState);
   };
-  
-  const Switch = ({
-    defaultEnable: enabledByDefault,
-    variant = "common",
-    disabled,
-    onChange,
-  }: SwitchProps) => {
-    return <div></div>;
-  };
-  
-  export default Switch;
+
+  return (
+    <HeadlessSwitch
+      checked={enabled}
+      onChange={toggle}
+      disabled={disabled}
+      {...rest}
+    >
+      {variant === "common" && <span />}
+      {variant === "contract" && (
+        <span>
+          {enabled && <CheckIcon aria-disabled={disabled} />}
+          {enabled || <XMarkIcon aria-disabled={disabled} />}
+        </span>
+      )}
+    </HeadlessSwitch>
+  );
+};
+
+export default Switch;
