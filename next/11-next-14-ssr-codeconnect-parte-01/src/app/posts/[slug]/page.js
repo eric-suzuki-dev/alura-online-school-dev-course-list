bd63e76a1f1af9c6237183cc64d646725315bb13
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-07-25 23:34:20 -0300

    Module 05.desafio 01

--- next/11-next-14-ssr-codeconnect-parte-01/src/app/posts/[slug]/page.js
+++ next/11-next-14-ssr-codeconnect-parte-01/src/app/posts/[slug]/page.js
@@ -1,3 +1,5 @@
+import logger from "@/logger"
+
 async function getPostBySlug (slug) {
     const url  = `http://localhost:3042/posts?slug=${slug}`
     const response = await fetch(url)
@@ -14,9 +16,9 @@ async function getPostBySlug (slug) {
     return data[0];
 } 
 
-const PagePost = () => {
-    const post = getPostBySlug(???)
-    return <h1>Olá posts</h1>
+const PagePost = async ({ params }) => {
+    const post = await getPostBySlug(params.slug)
+    return <h1 style={{ color: 'white' }}>{post.title}</h1>
 }
 
 export default PagePost
