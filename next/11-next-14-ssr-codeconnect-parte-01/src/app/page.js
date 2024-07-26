88d41a35b3651cbe53eff8b0fb43df5e37318016
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-07-25 23:19:35 -0300

    Module 03.update 03

--- next/11-next-14-ssr-codeconnect-parte-01/src/app/page.js
+++ next/11-next-14-ssr-codeconnect-parte-01/src/app/page.js
@@ -20,8 +20,10 @@ export default async function Home({ searchParams }) {
   return (
     <main className={styles.grid}>
       {posts.map(post =>  <CardPost key={post.id} post={post} />)}
-      {prev && <Link href={`/?page=${prev}`}>Página anterior</Link>}
-      {next && <Link href={`/?page=${next}`}>Próxima página</Link>}
+      <div className={styles.links}>
+        {prev && <Link href={`/?page=${prev}`}>Página anterior</Link>}
+        {next && <Link href={`/?page=${next}`}>Próxima página</Link>}
+      </div>
     </main>
   )
 }
