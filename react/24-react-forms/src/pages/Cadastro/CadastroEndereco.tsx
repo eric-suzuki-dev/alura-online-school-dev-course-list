473cccb859b161cc37099d32f619edfb6435bc50
eric-suzuki-dev <suzuki.y.eric@gmail.com>
2024-08-02 16:42:39 -0300

    Module 04.Lesson 01

--- react/24-react-forms/src/pages/Cadastro/CadastroEndereco.tsx
+++ react/24-react-forms/src/pages/Cadastro/CadastroEndereco.tsx
@@ -1,3 +1,4 @@
+import { useForm } from "react-hook-form";
 import {
   Button,
   Fieldset,
@@ -8,28 +9,61 @@ import {
   Titulo,
 } from "../../components";
 
+interface FormInputEndereco {
+  cep: string;
+  rua: string;
+  numero: string;
+  bairro: string;
+  localidade: string;
+}
+
 const CadastroEndereco = () => {
+  const { register, handleSubmit } = useForm<FormInputEndereco>();
+
+  const aoSubmeter = (dados: FormInputEndereco) => {
+    console.log(dados);
+  };
   return (
     <>
       <Titulo>Agora, mais alguns dados sobre você:</Titulo>
-      <Form>
+      <Form onSubmit={handleSubmit(aoSubmeter)}>
         <Fieldset>
           <Label htmlFor="campo-cep">CEP</Label>
-          <Input id="campo-cep" placeholder="Insira seu CEP" type="text" />
+          <Input
+            id="campo-cep"
+            placeholder="Insira seu CEP"
+            type="text"
+            {...register("cep")}
+          />
         </Fieldset>
         <Fieldset>
           <Label htmlFor="campo-rua">Rua</Label>
-          <Input id="campo-rua" placeholder="Rua Agarikov" type="text" />
+          <Input
+            id="campo-rua"
+            placeholder="Rua Agarikov"
+            type="text"
+            {...register("rua")}
+          />
         </Fieldset>
 
         <FormContainer>
           <Fieldset>
             <Label htmlFor="campo-numero-rua">Número</Label>
-            <Input id="campo-numero-rua" placeholder="Ex: 1440" type="text" />
+            <Input
+              id="campo-numero-rua"
+              placeholder="Ex: 1440"
+              type="text"
+              {...register("numero")}
+            />
           </Fieldset>
           <Fieldset>
             <Label htmlFor="campo-bairro">Bairro</Label>
-            <Input id="campo-bairro" placeholder="Vila Mariana" type="text" />
+            <Input
+              id="campo-bairro"
+              placeholder="Vila Mariana"
+              type="text"
+              {...register("bairro")}
+            />
           </Fieldset>
         </FormContainer>
         <Fieldset>
@@ -38,6 +72,7 @@ const CadastroEndereco = () => {
             id="campo-localidade"
             placeholder="São Paulo, SP"
             type="text"
+            {...register("localidade")}
           />
         </Fieldset>
         <Button type="submit">Cadastrar</Button>
