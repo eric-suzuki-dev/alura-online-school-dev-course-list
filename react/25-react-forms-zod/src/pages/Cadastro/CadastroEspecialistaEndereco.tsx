233d3680d21929440e2d83f5b84c3881b3abce57
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-08-04 00:21:02 -0300

    Module 05.Lesson 01

--- react/25-react-forms-zod/src/pages/Cadastro/CadastroEspecialistaEndereco.tsx
+++ react/25-react-forms-zod/src/pages/Cadastro/CadastroEspecialistaEndereco.tsx
@@ -22,6 +22,7 @@ import { useCallback, useEffect } from "react";
 const esquemaCadastroEnderecoEspecialista = z.object({
   endereco: z.object({
     cep: z.string().min(8, "Informe um CEP válido"),
+    avatar: z.instanceof(FileList).transform((lista) => lista.item(0)!),
     rua: z.string().min(1, "Informe uma rua válida"),
     numero: z.coerce.number().min(1, "Informe um número válido"),
     bairro: z.string().min(1, "Informe um bairro válido"),
@@ -52,6 +53,7 @@ const CadastroEspecialistaEndereco = () => {
     defaultValues: {
       endereco: {
         cep: "",
+        avatar: new File([""], "dummy.jpg", { type: "image/jpeg" }),
         rua: "",
         numero: 0,
         bairro: "",
@@ -64,17 +66,23 @@ const CadastroEspecialistaEndereco = () => {
     console.log(dados);
   };
 
-  const handleSetDados = useCallback((dados: EnderecoProps) => {
-    setValue("endereco.bairro", dados.bairro);
-    setValue("endereco.rua", dados.logradouro);
-    setValue("endereco.localidade", dados.localidade + ", " + dados.uf);
-  }, [setValue]);
+  const handleSetDados = useCallback(
+    (dados: EnderecoProps) => {
+      setValue("endereco.bairro", dados.bairro);
+      setValue("endereco.rua", dados.logradouro);
+      setValue("endereco.localidade", dados.localidade + ", " + dados.uf);
+    },
+    [setValue]
+  );
 
-  const buscaEndereco = useCallback(async (cep: string) => {
-    const result = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-    const dados = await result.json();
-    handleSetDados(dados);
-  }, [handleSetDados]);
+  const buscaEndereco = useCallback(
+    async (cep: string) => {
+      const result = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+      const dados = await result.json();
+      handleSetDados(dados);
+    },
+    [handleSetDados]
+  );
 
   const codigoCep = watch("endereco.cep");
 
@@ -92,7 +100,15 @@ const CadastroEspecialistaEndereco = () => {
           <UploadLabel htmlFor="campo-upload">
             <UploadIcon />
             <UploadDescription>Clique para enviar</UploadDescription>
-            <UploadInput accept="image/*" id="campo-upload" type="file" />
+            <UploadInput
+              accept="image/*"
+              id="campo-upload"
+              type="file"
+              {...register("endereco.avatar")}
+            />
+            {errors.endereco?.avatar && (
+              <ErrorMessage>{errors.endereco?.avatar.message}</ErrorMessage>
+            )}
           </UploadLabel>
         </>
 
