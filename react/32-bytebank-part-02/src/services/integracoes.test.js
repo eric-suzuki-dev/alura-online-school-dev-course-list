96b536b502c006e70b494a606b9f0760c1065a33
eric-suzuki-dev <suzuki.y.eric@gmail.com>
2024-08-14 16:24:58 -0300

    Module 03.Lesson 02

--- react/32-bytebank-part-02/src/services/integracoes.test.js
+++ react/32-bytebank-part-02/src/services/integracoes.test.js
@@ -1,15 +1,34 @@
-import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
-import App from '../paginas/Principal/App';
+import api from './api';
 import { buscaTransacoes } from './transacoes';
+
+jest.mock('./api');
+
+const mockTransacao = [
+  {
+    id: 1,
+    transacao: 'Depósito',
+    valor: '100',
+    data: '22/11/2022',
+    mes: 'Novembro',
+  },
+];
+
+const mockRequisicao = (retorno) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        data: retorno,
+      });
+    }, 200);
+  });
+};
 
 describe('Requisições para API', () => {
   test('Deve retornar uma lista de transações', async () => {
-    const transacoes = await buscaTransacoes();
-    expect(transacoes).toHaveLength(3);
+    api.get.mockImplementation(() => mockRequisicao(mockTransacao));
 
-    render(<App />, { wrapper: BrowserRouter });
-    const transacao = await screen.findAllByText('Novembro');
-    transacao.forEach(transacao => expect(transacao).toBeInTheDocument())
+    const transacoes = await buscaTransacoes();
+    expect(transacoes).toEqual(mockTransacao);
+    expect(api.get).toHaveBeenCalledWith('/transacoes');
   });
 });
