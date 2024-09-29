279f26471c92cb93c80d791b93a710a45beb3ef3
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-09-28 23:41:53 -0300

    Module 06.Lesson 02

--- javascript/20-javascript/js/main.js
+++ javascript/20-javascript/js/main.js
@@ -1,8 +1,20 @@
-
 import ui from "./ui.js"
 import api from "./api.js"
 
 const pensamentosSet = new Set()
+
+async function adicionarChaveAoPensamento() {
+  try {
+    const pensamentos = await api.buscarPensamentos()
+    pensamentos.forEach(pensamento => {
+      const chavePensamento = 
+      `${pensamento.conteudo.trim().toLowerCase()}-${pensamento.autoria.trim().toLowerCase()}`
+      pensamentosSet.add(chavePensamento)
+    })
+  } catch (error) {
+    alert('Erro ao adicionar chave ao pensamento')
+  }
+}
 
 function removerEspacos(string) {
   return string.replaceAll(/\s+/g, '')
@@ -22,6 +34,7 @@ function validarAutoria(autoria) {
 
 document.addEventListener("DOMContentLoaded", () => {
   ui.renderizarPensamentos()
+  adicionarChaveAoPensamento()
 
   const formularioPensamento = document.getElementById("pensamento-form")
   const botaoCancelar = document.getElementById("botao-cancelar")
