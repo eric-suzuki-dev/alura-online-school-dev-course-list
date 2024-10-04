c8d26a38ad83c5404b560317f6699081b4abf093
eric-suzuki-dev <suzuki.y.eric@gmail.com>
2024-10-04 14:48:57 -0300

    Module 02.Lesson 01

--- react/35-trato-tech-testes/src/pages/Home/Home.test.js
+++ react/35-trato-tech-testes/src/pages/Home/Home.test.js
@@ -1,5 +1,9 @@
+import { render } from '@testing-library/react';
+import Home from '.';
+
 describe('Testando página Home', () => {
   test('testando', () => {
+    render(<Home />);
     expect(true).toBeTruthy();
-  });
-});
+  })
+})
