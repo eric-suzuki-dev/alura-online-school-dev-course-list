1db45b1298eb411265e775be8e70b935410870b7
eric-suzuki-dev <suzuki.y.eric@gmail.com>
2024-10-04 14:54:48 -0300

    Module 03.Lesson 03

--- react/35-trato-tech-testes/src/pages/Home/Home.test.js
+++ react/35-trato-tech-testes/src/pages/Home/Home.test.js
@@ -1,11 +1,13 @@
 import Home from '.';
 import { render, screen } from '../../test-utils';
 
+jest.mock('services/categorias');
+
 describe('Testando página Home', () => {
   test('deve renderizar com categorias', async () => {
     render(<Home />);
     const categorias = await screen.findAllByTestId('home-categorias');
 
-    expect(categorias).toHaveLength(5);
+    expect(categorias).toHaveLength(2);
   })
 })
