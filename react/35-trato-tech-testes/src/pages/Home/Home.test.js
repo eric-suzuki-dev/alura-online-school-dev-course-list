27c254bc5bcb4acc80f0f96900c8b627ed9dca64
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-10-05 22:54:00 -0300

    Module 04.Lesson 01

--- react/35-trato-tech-testes/src/pages/Home/Home.test.js
+++ react/35-trato-tech-testes/src/pages/Home/Home.test.js
@@ -1,13 +1,27 @@
 import Home from '.';
 import { render, screen } from '../../test-utils';
+import userEvent from '@testing-library/user-event';
 
 jest.mock('services/categorias');
 
 describe('Testando página Home', () => {
-  test('deve renderizar com categorias', async () => {
-    render(<Home />);
-    const categorias = await screen.findAllByTestId('home-categorias');
+  describe('Anuncie', () => {
+    test('Deve redirecionar para a página anuncie', () => {
+      const botaoAnuncie = screen.getByTestId('home-botao-anunciar');
 
-    expect(categorias).toHaveLength(2);
+      userEvent.click(botaoAnuncie);
+
+      expect(?).toHaveBeenCalledWith('/anuncie');
+    })
   })
+
+  describe('Categorias', () => {
+    test('Deve renderizar com categorias', async () => {
+      render(<Home />);
+      const categorias = await screen.findAllByTestId('home-categorias');
+  
+      expect(categorias).toHaveLength(2);
+    }) 
+  })
+
 })
