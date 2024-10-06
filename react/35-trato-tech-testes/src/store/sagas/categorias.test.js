c0ab0dcf64c4048b6faf8d41a00e3e7a8808e441
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-10-06 20:19:31 -0300

    Module 05.Lesson 03

--- react/35-trato-tech-testes/src/store/sagas/categorias.test.js
+++ react/35-trato-tech-testes/src/store/sagas/categorias.test.js
@@ -1,6 +1,7 @@
-import { call } from 'redux-saga/effects';
-import { observarCategorias } from './categorias';
+import { call, cancel, take } from 'redux-saga/effects';
+import { categoriasSaga, observarCategorias } from './categorias';
 import categoriasService from 'services/categorias';
+import { adicionarTodasAsCategorias } from 'store/reducers/categorias';
 
 describe('Testando categorias saga', () => {
   describe('workers', () => {
@@ -13,6 +14,17 @@ describe('Testando categorias saga', () => {
       const funcaoExecutada = funcaoGeradora.next();
 
       expect(funcaoExecutada.value).toEqual(funcaoEsperada);
+    })
+  })
+  describe('watchers', () => {
+    test('Deve executar corretamente', () => {
+      const funcaoGeradora = categoriasSaga();
+      const tarefa = funcaoGeradora.next();
+      const funcaoEsperada = take(adicionarTodasAsCategorias);
+      const funcaoCancelarEsperada = cancel(tarefa.value);
+
+      expect(funcaoGeradora.next().value).toEqual(funcaoEsperada);
+      expect(funcaoGeradora.next().value).toEqual(funcaoCancelarEsperada);
     })
   })
 })
