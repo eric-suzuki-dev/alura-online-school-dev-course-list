482c864490c711196328b7661118dbac706c6e9a
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-10-19 21:35:53 -0300

    Module 01.Lesson 03

--- react/37-react-pwa-part-02/src/App.jsx
+++ react/37-react-pwa-part-02/src/App.jsx
@@ -2,12 +2,14 @@ import Header from "./components/Header";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
 import useNotification from "./hooks/useNotification";
+import NotificationButton from "./components/NotificationButton";
 
 function App() {
-  const {} = useNotification()
+  const {} = useNotification();
   return (
     <>
       <Header />
+      <NotificationButton/>
       <Outlet />
       <Footer />
     </>
