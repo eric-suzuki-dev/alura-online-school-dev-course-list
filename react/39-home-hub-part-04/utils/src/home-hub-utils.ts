e8b298659c3f1f3de3c7302dd61c775d02e23778
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2025-01-08 23:50:31 -0300

    Module 02.Lesson 03

--- react/39-home-hub-part-04/utils/src/home-hub-utils.ts
+++ react/39-home-hub-part-04/utils/src/home-hub-utils.ts
@@ -1,3 +1,5 @@
+import Cookies from 'universal-cookie';
+
 export type AuthInfo = {
 	email: string;
 	authId: number;
@@ -5,36 +7,9 @@ export type AuthInfo = {
 	lastName?: string;
 };
 
-export const loginFunction = (email: AuthInfo['email']) => {
+export function loginFunction(email: AuthInfo['email']) {
 	const authId = email.replace('@', '').replace('.', '').codePointAt(0);
-	localStorage.setItem('auth', JSON.stringify({ email: email, authId: authId }));
+	const cookies = new Cookies(null, { path: '/' });
+	cookies.set('auth', { email: email, authId: authId });
 	location.replace(`/dashboard/${authId}/`);
-};
-
-export const logoutFunction = () => {
-	localStorage.removeItem('auth');
-	return location.replace('/');
-};
-
-export const checkIsAuthenticated = () => {
-	const auth = localStorage.getItem('auth');
-	if (!auth) {
-		return { authInfo: undefined, isAuthenticated: false };
-	}
-	const authObj: AuthInfo = JSON.parse(auth);
-	if (!location.pathname.includes(authObj.authId.toString())) {
-		return { authInfo: undefined, isAuthenticated: false };
-	}
-	return { authInfo: authObj, isAuthenticated: true };
-};
-
-export const editAuthInfo = (data: AuthInfo) => {
-	const auth = JSON.parse(localStorage.getItem('auth'));
-	if (!location.pathname.includes(auth.authId.toString())) {
-		alert('Você não tem permissão para editar este usuário');
-		return logoutFunction();
-	}
-	localStorage.setItem('auth', JSON.stringify(data));
-	alert('Usuário editado com sucesso');
-	return;
-};
+}
