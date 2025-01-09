a05f5e06550cf10be93788b330c223ad2dfd81ab
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2025-01-09 02:22:11 -0300

    Module 03.Lesson 01

--- react/39-home-hub-part-04/react-utils/src/home-hub-react-utils.tsx
+++ react/39-home-hub-part-04/react-utils/src/home-hub-react-utils.tsx
@@ -1,4 +1,34 @@
-// Anything exported from this file is importable by other in-browser modules.
-export function helloWorld() {
-	console.log('Hello, World!');
+import Cookies from 'universal-cookie';
+
+export type AuthInfo = {
+	email: string;
+	authId: number;
+	firstName?: string;
+	lastName?: string;
+};
+
+export function loginFunction(email: AuthInfo['email']) {
+	const authId = email.replace('@', '').replace('.', '').codePointAt(0);
+	const cookies = new Cookies(null, { path: '/' });
+	cookies.set('auth', { email: email, authId: authId });
+	location.replace(`/dashboard/${authId}/`);
+}
+
+export function logoutFunction() {
+	const cookies = new Cookies(null, { path: '/' });
+	cookies.remove('auth');
+	return location.replace('/');
+}
+
+export function checkIsAuthenticated() {
+	const cookies = new Cookies(null, { path: '/' });
+	const auth = cookies.get('auth');
+	if (!auth) {
+		return { authInfo: undefined, isAuthenticated: false };
+	}
+	const authObj: AuthInfo = JSON.parse(auth);
+	if (!location.pathname.includes(authObj.authId.toString())) {
+		return { authInfo: undefined, isAuthenticated: false };
+	}
+	return { authInfo: authObj, isAuthenticated: true };
 }
