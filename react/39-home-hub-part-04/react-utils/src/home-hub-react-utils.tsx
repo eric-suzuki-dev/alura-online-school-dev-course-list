f2327404fac09de7a2aaeea2ba9be1280cb9a4bf
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2025-01-09 02:22:45 -0300

    Module 03.Lesson 02

--- react/39-home-hub-part-04/react-utils/src/home-hub-react-utils.tsx
+++ react/39-home-hub-part-04/react-utils/src/home-hub-react-utils.tsx
@@ -23,6 +23,7 @@ export function logoutFunction() {
 export function checkIsAuthenticated() {
 	const cookies = new Cookies(null, { path: '/' });
 	const auth = cookies.get('auth');
+	console.log('COOKIE AUTH: ', auth);
 	if (!auth) {
 		return { authInfo: undefined, isAuthenticated: false };
 	}
@@ -31,4 +32,16 @@ export function checkIsAuthenticated() {
 		return { authInfo: undefined, isAuthenticated: false };
 	}
 	return { authInfo: authObj, isAuthenticated: true };
+}
+
+export function editAuthInfo(data: AuthInfo) {
+	const cookies = new Cookies(null, { path: '/' });
+	const auth = cookies.get('auth');
+	if (!location.pathname.includes(auth.authId.toString())) {
+		alert('Você não tem permissão para editar este usuário');
+		return logoutFunction();
+	}
+	cookies.set('auth', data);
+	alert('Usuário editado com sucesso');
+	return;
 }
