4e2764ad442acc9b76f1d99ba120ba74176762c5
Eric Y. Suzuki <142278417+eric-suzuki-dev@users.noreply.github.com>
2025-02-08 23:20:44 -0300

    Module 03.Lesson 02

--- react/42-home-hub-part-05/react-parcel/src/parcel.component.tsx
+++ react/42-home-hub-part-05/react-parcel/src/parcel.component.tsx
@@ -1,4 +1,52 @@
-export default function Parcel(props) {
-	console.log('Parcel props:', props);
-	return <section>Hello World!</section>;
+import * as React from 'react';
+
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+import { TransitionProps } from '@mui/material/transitions';
+
+const Transition = React.forwardRef(function Transition(
+	props: TransitionProps & {
+		children: React.ReactElement<any, any>;
+	},
+	ref: React.Ref<unknown>
+) {
+	return <Slide direction='up' ref={ref} {...props} />;
+});
+
+export default function Parcel() {
+	const [open, setOpen] = React.useState(false);
+
+	const handleClickOpen = () => {
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+
+	return (
+		<React.Fragment>
+			<Dialog
+				open={open}
+				TransitionComponent={Transition}
+				keepMounted
+				onClose={handleClose}
+				aria-describedby='alert-dialog-slide-description'
+			>
+				<DialogTitle>{title}</DialogTitle>
+				<DialogContent>
+					<DialogContentText id='alert-dialog-slide-description'>{description}</DialogContentText>
+				</DialogContent>
+				<DialogActions>
+					<Button onClick={handleClose}>{leftBtnText}</Button>
+					<Button onClick={handleClose}>{rightBtnText}</Button>
+				</DialogActions>
+			</Dialog>
+		</React.Fragment>
+	);
 }
