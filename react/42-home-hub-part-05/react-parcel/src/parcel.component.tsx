c9e6110f316f8bd6e70086117ffa151739168493
Eric Y. Suzuki <142278417+eric-suzuki-dev@users.noreply.github.com>
2025-02-09 21:18:39 -0300

    Module 03.Lesson 03

--- react/42-home-hub-part-05/react-parcel/src/parcel.component.tsx
+++ react/42-home-hub-part-05/react-parcel/src/parcel.component.tsx
@@ -9,6 +9,16 @@ import DialogTitle from '@mui/material/DialogTitle';
 import Slide from '@mui/material/Slide';
 import { TransitionProps } from '@mui/material/transitions';
 
+interface ParcelProps {
+	title: string;
+	description: string;
+	leftBtnFn: () => void;
+	rightBtnFn: () => void;
+	leftBtnText: string;
+	rightBtnText: string;
+	isOpen: boolean;
+}
+
 const Transition = React.forwardRef(function Transition(
 	props: TransitionProps & {
 		children: React.ReactElement<any, any>;
@@ -18,35 +28,43 @@ const Transition = React.forwardRef(function Transition(
 	return <Slide direction='up' ref={ref} {...props} />;
 });
 
-export default function Parcel() {
-	const [open, setOpen] = React.useState(false);
+export default function Parcel({
+	description,
+	isOpen,
+	leftBtnFn,
+	leftBtnText,
+	rightBtnFn,
+	rightBtnText,
+	title,
+}: ParcelProps) {
+	const [open, setOpen] = React.useState(isOpen);
 
-	const handleClickOpen = () => {
-		setOpen(true);
+	const _leftBtnFn = () => {
+		leftBtnFn();
+		setOpen(false);
 	};
 
-	const handleClose = () => {
+	const _rightBtnFn = () => {
+		rightBtnFn();
 		setOpen(false);
 	};
 
 	return (
-		<React.Fragment>
-			<Dialog
-				open={open}
-				TransitionComponent={Transition}
-				keepMounted
-				onClose={handleClose}
-				aria-describedby='alert-dialog-slide-description'
-			>
-				<DialogTitle>{title}</DialogTitle>
-				<DialogContent>
-					<DialogContentText id='alert-dialog-slide-description'>{description}</DialogContentText>
-				</DialogContent>
-				<DialogActions>
-					<Button onClick={handleClose}>{leftBtnText}</Button>
-					<Button onClick={handleClose}>{rightBtnText}</Button>
-				</DialogActions>
-			</Dialog>
-		</React.Fragment>
+		<Dialog
+			open={open}
+			TransitionComponent={Transition}
+			keepMounted
+			onClose={() => setOpen(false)}
+			aria-describedby='alert-dialog-slide-description'
+		>
+			<DialogTitle>{title}</DialogTitle>
+			<DialogContent>
+				<DialogContentText id='alert-dialog-slide-description'>{description}</DialogContentText>
+			</DialogContent>
+			<DialogActions>
+				<Button onClick={_leftBtnFn}>{leftBtnText}</Button>
+				<Button onClick={_rightBtnFn}>{rightBtnText}</Button>
+			</DialogActions>
+		</Dialog>
 	);
 }
